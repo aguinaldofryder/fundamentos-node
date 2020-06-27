5da68e0a0749f49a7fb68492fa5def5552edefeb
Aguinaldo Fryder <aguinaldofryder@hotmail.com>
2020-06-27 11:26:52 -0300

    Implements all functions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,61 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.reduceValueTransactionByType('income');
+    const outcome = this.reduceValueTransactionByType('outcome');
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction: Transaction = {
+      id: uuid(),
+      title,
+      value,
+      type,
+    };
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+
+  private reduceValueTransactionByType(type: 'income' | 'outcome'): number {
+    const checkTransaction = this.getCheckTransactionByType(type);
+
+    const total = this.transactions
+      .filter(checkTransaction)
+      .reduce(this.agregateValueTransaction, 0);
+
+    return total;
+  }
+
+  private getCheckTransactionByType(
+    type: 'income' | 'outcome',
+  ): (transaction: Transaction) => boolean {
+    return type === 'income'
+      ? this.checkTransactionTypeIsIncome
+      : this.checkTransactionTypeIsOutcome;
+  }
+
+  private checkTransactionTypeIsIncome(transaction: Transaction): boolean {
+    return transaction.type === 'income';
+  }
+
+  private checkTransactionTypeIsOutcome(transaction: Transaction): boolean {
+    return transaction.type === 'outcome';
+  }
+
+  private agregateValueTransaction(
+    total: number,
+    transaction: Transaction,
+  ): number {
+    return total + transaction.value;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,29 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    this.checkAmountLessThanBalance(type, value, total);
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
+  }
+
+  private checkAmountLessThanBalance(
+    type: string,
+    value: number,
+    total: number,
+  ): void {
+    if (type === 'outcome' && value > total) {
+      throw new Error(
+        'The amount posted is greater than the amount avaliable in cash',
+      );
+    }
   }
 }
 
